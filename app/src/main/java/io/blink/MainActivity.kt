6d7a559000c562de7e7283caa86205e8b7a5f878
Egor Salnikov <EDSalnikov@detmir.tu>
2021-11-23 10:28:30 +0300

    Change super class from Component to AppCompat

--- app/src/main/java/io/blink/MainActivity.kt
+++ app/src/main/java/io/blink/MainActivity.kt
@@ -1,9 +1,9 @@
 package io.blink
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
