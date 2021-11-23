92ca122a16a78f5e910f59d4eaa0a00a5d4fee23
Egor Salnikov <EDSalnikov@detmir.tu>
2021-11-23 09:15:31 +0300

    Migrate gradle from groovy to kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,16 @@ buildscript {
         google()
         mavenCentral()
     }
+
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.3"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0"
+        classpath("com.android.tools.build:gradle:7.0.3")
+        classpath(kotlin("gradle-plugin", version = "1.6.0"))
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
