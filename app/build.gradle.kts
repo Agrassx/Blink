cc395c502e440f2ec785ccaa17934dcf1a943fbc
Egor Salnikov <EDSalnikov@detmir.tu>
2021-11-23 10:52:35 +0300

    Add activity-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
 
     testImplementation("junit:junit:4.+")
